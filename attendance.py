56188839496c5e9af32ec5462a0118134345ebb8
Javier Puebla <jpuebla1993@gmail.com>
2020-05-16 18:35:10 -0400

    add Attendance class to manage all attendance releated methods

--- attendance.py
+++ attendance.py
@@ -26,58 +26,42 @@ def parse_args():
     parser.add_argument('-f', '--folder', type=folder, default="./", help='The path of the folder to store the result. Defaults to "./"')
     return parser.parse_args()
 
-def main(subject, classtype):
+def main():
     args = parse_args()
 
     camera = cvwindows.create("Camera")
-
-    date = datetime.date.today()
-    asist = { "date": date, "students":[], "course": args.topic}
 
     capture = cv2.VideoCapture(args.camera)
     _ , image = capture.read()
     if image is None:
         print ("Invalid camera index. Use -c option to choose a correct camera on your system.")
         sys.exit(1)
+
     h, w, _ = image.shape
-    scanner = QRScanner(w,h)
+    scanner = QRScanner(w, h)
+
+    attendance_so_far = []
+    db = Attendance.get_data_base()
 
     while cvwindows.event_loop():
-        _ , image = capture.read()
-        gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        camera.show(image)
-        result = scanner.get_qrcodes(gray_image)
+            _ , image = capture.read()
+            camera.show(image)
 
-        if len(result)==0: continue
+            qrs = Attendance.get_qrcodes(image, scanner)
+            students = Attendance.get_student_from_qrcode(qrs, attendance_so_far)
 
-        for qr in result:
-            if not valid_qrcode(qr.data): continue
-            student = get_student_info(qr.data)
-
-            scanned = False
-            for s in asist["students"]:
-                if s["ID"] == student["ID"]: scanned= True
-
-            if not scanned:
-                asist["students"].append(student)
-                beep.beep()
-
-    # file_path = os.path.join(args.folder, "attendance-%s-%s.json"%(date,args.topic))
-    # archive = open(file_path, "w")
-    # json.dump(asist, archive, indent=4)
-
-    db = sqlite3.connect('attendance.db')
-    db.execute('''CREATE TABLE IF NOT EXISTS attendance
-             (id integer, date integer, subject text, classtype text, name text, uploaded boolean)''')
+            Attendance.insert_attendance_into_data_base(students, class_details, db)
+            attendance_so_far.extend(students)
+    
     for student in asist["students"]:
         db.execute('''INSERT INTO attendance VALUES (?, ?, ?, ?, ?, 'False')''', 
                     [student["ID"], date, subject, classtype, student["Name"]])
 
-    # Save (commit) the changes
-    db.commit()
-    db.close()
+        # Save (commit) the changes
+        db.commit()
+        db.close()
 
-    # requests.post('http://127.0.0.1:5000', data = {'datetime': date, 'teacher' : 'dvd', 'signature' : 'AC', 'list' : j})
+        # requests.post('http://127.0.0.1:5000', data = {'datetime': date, 'teacher' : 'dvd', 'signature' : 'AC', 'list' : j})
 
 class QRCode(object):
     """
@@ -112,47 +96,107 @@ class QRScanner(object):
     def cv2_to_zbar_image(self, cv2_image):
         return zbar.Image(self.width, self.height, 'Y800',cv2_image.tostring())
 
-def valid_qrcode(qrcode_data):
+class Attendance:
 
-    qrcode_data = qrcode_data.split("\n")
-    if len(qrcode_data) != 5 : return False
+    @staticmethod
+    def get_data_base():
+        db = sqlite3.connect('attendance.db')
+        db.execute('''CREATE TABLE IF NOT EXISTS attendance
+                (id integer, date timestamp, courseName text, classType text, name text, uploaded boolean, details text)''')
 
-    #N: Nombre
-    test = qrcode_data[0].split(":")
-    if test[0] != "N" or len(test[1]) == 0: return False
+        return db
 
-    #A\: Apellidos
-    test = qrcode_data[1].split(":")
-    if test[0] != "A" or len(test[1]) == 0: return False
+    @staticmethod
+    def valid_qrcode(qrcode_data):
 
-    #CI\: 12345678901
-    test = qrcode_data[2].split(":")
-    if test[0] != "CI" or len(test[1].strip()) != 11: return False
+        qrcode_data = qrcode_data.split("\n")
+        if len(qrcode_data) != 5 : return False
 
-    #FV\: AA0000000
-    test = qrcode_data[3].split(":")
-    if test[0] != "FV" or len(test[1].strip()) != 9: return False
+        #N: Nombre
+        test = qrcode_data[0].split(":")
+        if test[0] != "N" or len(test[1]) == 0: return False
 
-    return True
+        #A\: Apellidos
+        test = qrcode_data[1].split(":")
+        if test[0] != "A" or len(test[1]) == 0: return False
 
-def get_student_info(qrcode_data):
-    # get qrcode info
-    qrcode_data = qrcode_data.split("\n")
+        #CI\: 12345678901
+        test = qrcode_data[2].split(":")
+        if test[0] != "CI" or len(test[1].strip()) != 11: return False
 
-    # get student sex
-    # if int(qrcode_data[2][-2]) % 2 == 0:
-    #     gender = "male"
-    # else:
-    #     gender = "female"
+        #FV\: AA0000000
+        test = qrcode_data[3].split(":")
+        if test[0] != "FV" or len(test[1].strip()) != 9: return False
 
-    return {
-                "ID": qrcode_data[2][-12:].strip(),
-                "Name": qrcode_data[0][2:].strip() + " " + qrcode_data[1][2:].strip(),
-                # "Name": qrcode_data[0][2:].strip(),
-                # "LastName": qrcode_data[1][2:].strip(),
-                # "FV": qrcode_data[3][-9:].strip(),
-                # "Gender": gender,
-            }
+        return True
+
+    @staticmethod
+    def get_student_info(qrcode_data):
+        # get qrcode info
+        qrcode_data = qrcode_data.split("\n")
+
+        # get student sex
+        # if int(qrcode_data[2][-2]) % 2 == 0:
+        #     gender = "male"
+        # else:
+        #     gender = "female"
+
+        return {
+                    "ID": qrcode_data[2][-12:].strip(),
+                    "Name": qrcode_data[0][2:].strip() + " " + qrcode_data[1][2:].strip(),
+                    # "Name": qrcode_data[0][2:].strip(),
+                    # "LastName": qrcode_data[1][2:].strip(),
+                    # "FV": qrcode_data[3][-9:].strip(),
+                    # "Gender": gender,
+                }
+
+    @staticmethod
+    def get_qrcodes(image, scanner=None):
+        if scanner is None:
+            h, w, _ = image.shape
+            scanner = QRScanner(w, h)
+
+        gray_image = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+        qrs = scanner.get_qrcodes(gray_image)
+
+        return qrs
+
+    @staticmethod
+    def get_student_from_qrcode(qrs, attendance_so_far):
+        students = []
+        for qr in qrs:
+            if not Attendance.valid_qrcode(qr.data):
+                continue
+
+            student = Attendance.get_student_info(qr.data)
+            # check if the student has already been inserted in the database
+            if not student["ID"] in attendance_so_far:
+                students.append(student["ID"])
+                beep.beep()
+
+        return students
+
+    @staticmethod
+    def insert_attendance_into_data_base(student, class_details, db):
+        date = datetime.datetime.now()
+
+        to_insert = [
+            student["ID"], 
+            date, 
+            class_details['course_name'], 
+            class_details['class_type'], 
+            student["Name"], 
+            class_details['details']
+        ]
+        db.execute('''INSERT INTO attendance VALUES (?, ?, ?, ?, ?, 'False', ?)''', to_insert)
+        # Save (commit) the changes
+        db.commit()
+
+    @staticmethod
+    def pending_uploaded(db):
+        cur = db.execute("SELECT COUNT(*) FROM attendance WHERE uploaded = 'False'")
+        count = cur.fetchone()
+        return count[0]
 
 if __name__ == '__main__':
     # main() 
